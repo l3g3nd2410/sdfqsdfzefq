69eb4e01704d034f161ac7c85612e1650dc88d35
l3g3nd <162813613+l3g3nd2410@users.noreply.github.com>
2025-08-20 04:43:31 +0200

    Update AntiVoiceChatBan1.lua

--- AntiVoiceChatBan1.lua
+++ AntiVoiceChatBan1.lua
@@ -1,13 +1,27 @@
 local function AntiBan()
+
     print("✅ Script VoiceChat Auto-Rejoin lancé !")
     
     local lastLog = tick()
     
     while true do
-        task.wait()
-        pcall(function()
-            game:GetService("VoiceChatService"):joinVoice()
+        task.wait(1)
+    
+        local success, inVoice = pcall(function()
+            return game:GetService("VoiceChatService"):IsInVoice()
         end)
+    
+        if success and not inVoice then
+            local ok, err = pcall(function()
+                game:GetService("VoiceChatService"):JoinVoice()
+            end)
+    
+            if ok then
+                print("🔄 Tentative de connexion au VoiceChat...")
+            else
+                warn("⚠️ Erreur lors de JoinVoice :", err)
+            end
+        end
     
         if tick() - lastLog >= 5 then
             print("⏳ Script VoiceChat tourne toujours...")
