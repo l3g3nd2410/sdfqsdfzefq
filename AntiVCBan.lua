9077ec8e199cbf7921a8fbb764c12e70e8635fa0
l3g3nd <162813613+l3g3nd2410@users.noreply.github.com>
2025-08-20 04:33:47 +0200

    Update AntiVCBan.lua

--- AntiVCBan.lua
+++ AntiVCBan.lua
@@ -1,17 +1,21 @@
-local VoiceChatService = game:GetService("VoiceChatService")
-
-print("✅ Script VoiceChat Auto-Rejoin lancé !")
-
-local lastLog = tick()
-
-while true do
-    task.wait()
-    pcall(function()
-        VoiceChatService:JoinVoice()
-    end)
-
-    if tick() - lastLog >= 5 then
-        print("⏳ Script VoiceChat tourne toujours...")
-        lastLog = tick()
+local function AntiBan()
+    local VoiceChatService = game:GetService("VoiceChatService")
+    
+    print("✅ Script VoiceChat Auto-Rejoin lancé !")
+    
+    local lastLog = tick()
+    
+    while true do
+        task.wait()
+        pcall(function()
+            VoiceChatService:JoinVoice()
+        end)
+    
+        if tick() - lastLog >= 5 then
+            print("⏳ Script VoiceChat tourne toujours...")
+            lastLog = tick()
+        end
     end
 end
+
+AntiBan()
