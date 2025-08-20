a642362cf304b39604ae8cd9b5bf5d96b607c62b
l3g3nd <162813613+l3g3nd2410@users.noreply.github.com>
2025-08-20 06:03:35 +0200

    Update AntiVoiceChatBan.lua

--- AntiVoiceChatBan.lua
+++ AntiVoiceChatBan.lua
@@ -10,7 +10,7 @@ local function AntiBan()
     -- Conteneur bouton
     local button = Instance.new("TextButton")
     button.Name = "VoiceButton"
-    button.Size = UDim2.new(0, 130, 0, 42)
+    button.Size = UDim2.new(0, 160, 0, 50)
     button.Position = UDim2.new(0, 15, 0, 15)
     button.BackgroundTransparency = 1 -- transparent, on mettra un frame comme fond
     button.AutoButtonColor = false
@@ -20,7 +20,7 @@ local function AntiBan()
     -- Fond arrondi + gradient
     local bg = Instance.new("Frame")
     bg.Size = UDim2.new(1, 0, 1, 0)
-    bg.BackgroundColor3 = Color3.fromRGB(80, 80, 80)
+    bg.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
     bg.Parent = button
     
     local corner = Instance.new("UICorner")
